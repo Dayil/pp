dfd67bbae848a4f64950714069fcaf9f87526ebb
Daniel dos Santos Marfim <35313917+Dayil@users.noreply.github.com>
2026-01-31 17:45:49 -0300

    Delete .py básico directory

--- .py básico/Python Básico - Olá mundo/olamundo.py
+++
@@ -1,9 +0,0 @@
-# Olá mundo em Python após digitar 1
-
-while True:
-    entrada = input("Digite 1 para ver a mensagem 'Olá mundo': ")
-    if entrada == '1':
-        print("Olá mundo! 😉")
-        break
-    else:
-        print("Entrada inválida. Por favor, tente novamente.")
